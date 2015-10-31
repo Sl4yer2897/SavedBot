83e9fa00f03da4481012be071a0bfa76ca5b9f2a
Sl4yer2897 <cam.xbox360@hotmail.com>
2015-10-31 14:48:52 +1100

    Added Type Table
    Added a new table that displays the number of saved links and saved text
    posts

--- savedBot.py
+++ savedBot.py
@@ -18,12 +18,13 @@ while login == False:
         r.login(username, password, disable_warning=True)
         login = True
     except praw.errors.InvalidUserPass:
-        print('Invalid Password')
+        print('Invalid Username or Password')
 
-limit = int(input('How many saved posts do you want to retrieve? (Default is all)')) or None
+limit = int(input('How many saved posts do you want to retrieve (Default is all): ')) or None
 
 saved = list(r.user.get_saved(limit=limit))
 saved_subs = {}
+saved_type = {'Link': 0, 'Text': 0}
 
 for post in saved:
     url = post.permalink
@@ -36,9 +37,21 @@ for post in saved:
     else:
         saved_subs[subreddit] = 1
 
+    if type(post) == praw.objects.Submission:
+        saved_type['Link'] += 1
+    else:
+        saved_type['Text'] += 1
+
+
+
 sub_table = PrettyTable(['Subreddit', 'Saved Posts'])
+type_table = PrettyTable(['Type', 'Saves'])
 
 for key, value in saved_subs.items():
     sub_table.add_row([key, value])
 
+for key, value in saved_type.items():
+    type_table.add_row([key, value])
+
 print(sub_table.get_string(sortby="Saved Posts", reversesort=True))
+print(type_table.get_string(sortby="Type", reversesort=False))
